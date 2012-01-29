11b6597a8796b9004455258b0fa1b210f173b072
Nat Welch <nat@natwelch.com>
2012-01-28 18:16:01 -0800

    Cloning works, although I can't draw the clones.

--- lib/duperun/game_window.rb
+++ lib/duperun/game_window.rb
@@ -19,28 +19,32 @@ class GameWindow < Gosu::Window
 
     @map = Map.new(self, "media/lvl1.txt")
 
-    @player = Player.new(self, 400, 200)
+    @players = [Player.new(self, 400, 200)]
+    @current_player = 0
 
     @camera_x = @camera_y = 0
   end
 
   def update
-    @player.accelerate_left   if button_down? Gosu::KbLeft or button_down? Gosu::KbA or button_down? Gosu::GpLeft
-    @player.accelerate_right  if button_down? Gosu::KbRight or button_down? Gosu::KbD  or button_down? Gosu::GpRight
+    player.accelerate_left   if button_down? Gosu::KbA or button_down? Gosu::GpLeft
+    player.accelerate_right  if button_down? Gosu::KbD or button_down? Gosu::GpRight
 
-    @camera_x = [ [@player.x - @@center[0], 0].max,  @map.width * 50 - CONFIG[:window][:width]  ].min
-    @camera_y = [ [@player.y - @@center[1], 0].max, @map.height * 50 - CONFIG[:window][:height] ].min
+    @camera_x = [ [player.x - @@center[0], 0].max,  @map.width * 50 - CONFIG[:window][:width]  ].min
+    @camera_y = [ [player.y - @@center[1], 0].max, @map.height * 50 - CONFIG[:window][:height] ].min
 
-    @player.move
+    player.move
   end
 
 
   def draw
-    #DupeRun.log [@camera_x, @camera_y, @player.x, @player.y].inspect
+    #DupeRun.log [@camera_x, @camera_y, player.x, player.y].inspect
+    @players.each do |pl|
+      pl.draw if @players.index pl != @current_player
+    end
 
     self.translate(-@camera_x, -@camera_y) do
       @map.draw
-      @player.draw
+      player.draw
     end
   end
 
@@ -49,6 +53,23 @@ class GameWindow < Gosu::Window
     # The all important GTFO button.
     close if id == Gosu::KbEscape
 
-    @player.jump if id == Gosu::KbUp or id == Gosu::KbSpace
+    self.player.jump if id == Gosu::KbSpace
+    self.new_player(self.player) if id == Gosu::KbW
+    self.next_player if id == Gosu::KbTab
+  end
+
+  def player
+    return @players[@current_player]
+  end
+
+  def next_player
+    @current_player = (@current_player + 1) % @players.length
+  end
+
+  def new_player pl
+    @players.push(Player.new(self, pl.x, pl.y))
+    @current_player = @players.length - 1
+
+    return
   end
 end
